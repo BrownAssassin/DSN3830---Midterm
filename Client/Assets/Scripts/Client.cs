02c7ea1bf471fdc3883592d8052cac8f85ff89bb
Mrinank S <mrinank.sivakumar@ontariotechu.net>
2022-03-29 02:04:02 -0400

    Last Project Commit

--- Client/Assets/Scripts/Client.cs
+++ Client/Assets/Scripts/Client.cs
@@ -1,33 +1,254 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Non-Unity Libraries
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
 public class Client : MonoBehaviour
 {
+    private bool IPPass = false;
+    private bool readyState = false;
+    private bool SessionPass = false;
+    private int clientNum;
     private string serverIP = "";
+    private static Socket tcpSocket;
+    private static Socket udpSocket;
+    private static IPEndPoint remoteTCPEP;
+    private static EndPoint remoteUDPEP;
+    private static byte[] TCP_inBuffer = new byte[32];
+    private static byte[] TCP_outBuffer = new byte[32];
+    private static byte[] UDP_inBuffer = new byte[12];
+    private static byte[] UDP_outBuffer = new byte[12];
+    private static float[] playerPosition = new float[12];
+    private static float[] otherPosition = new float[12];
+    private static int recv = 0;
+    private float elapsedTime;
 
-    public GameObject InputField;
+    // Start Screen
     public GameObject StartScreen;
-    public Transform playerCube;
+    public GameObject InputField;
+    public GameObject IPError;
+    // Waiting Screen
+    public GameObject WaitingScreen;
+    public GameObject WaitingText;
+    // Prefabs & Players
+    public GameObject BluePrefab;
+    public GameObject GreenPrefab;
+    public GameObject playerCube;
+    public GameObject otherCube;
 
     public void GetServerIP()
     {
         serverIP = InputField.GetComponent<Text>().text;
+
+        StartClient();
+    }
+
+    public void StartClient()
+    {
+        try
+        {
+            IPAddress ip = IPAddress.Parse(serverIP);
+
+            remoteTCPEP = new IPEndPoint(ip, 11111);
+            remoteUDPEP = (EndPoint)new IPEndPoint(ip, 11112);
+
+            tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            Debug.Log("Connecting to server...");
+
+            tcpSocket.Connect(remoteTCPEP);
+
+            Debug.Log($"Connected to server IP: {tcpSocket.RemoteEndPoint.ToString()}");
+            Debug.Log("--------------------------------------------");
+
+            udpSocket.SendTo(Encoding.UTF8.GetBytes("connected"), remoteUDPEP);
+            int recv = tcpSocket.Receive(TCP_inBuffer);
+            clientNum = int.Parse(Encoding.UTF8.GetString(TCP_inBuffer, 0, recv));
+
+            tcpSocket.Blocking = false;
+            udpSocket.Blocking = false;
+
+            StartScreen.SetActive(false);
+            WaitingScreen.SetActive(true);
+            IPPass = true;
+        }
+        catch (ArgumentNullException anexc)
+        {
+            IPPass = false;
+            IPError.GetComponent<Text>().text = "~ No IP Address Entered ~";
+            Debug.Log($"ArgumentNullException: {anexc.ToString()}");
+        }
+        catch (FormatException fexc)
+        {
+            IPPass = false;
+            IPError.GetComponent<Text>().text = "~ Incorect IP Address Entered ~";
+            Debug.Log($"FormatException: {fexc.ToString()}");
+        }
+        catch (SocketException se)
+        {
+            IPPass = false;
+            IPError.GetComponent<Text>().text = "~ Socket Exception Encountered ~";
+            Debug.Log($"SocketException: {se.ToString()}");
+        }
+        catch (Exception e)
+        {
+            IPPass = false;
+            Debug.Log($"Unexpected Exception: {e.ToString()}");
+        }
+    }
+
+    public void SetupSession()
+    {
+        try
+        {
+            if (clientNum == 1)
+            {
+                WaitingText.GetComponent<Text>().text = $"Connected to server IP: {tcpSocket.RemoteEndPoint.ToString()} --------------------------------------------                Waiting for Client 2";
+            }
+            else if (clientNum == 2)
+            {
+                WaitingText.GetComponent<Text>().text = $"Connected to server IP: {tcpSocket.RemoteEndPoint.ToString()} --------------------------------------------                Waiting for Client 1";
+            }
+
+            if (!readyState)
+            {
+                tcpSocket.Send(Encoding.UTF8.GetBytes("Ready"));
+                readyState = true;
+            }
+
+            if (tcpSocket.Available > 0)
+            {
+                int recv = tcpSocket.Receive(TCP_inBuffer);
+                if (Encoding.UTF8.GetString(TCP_inBuffer, 0, recv) == "StartSession")
+                {
+                    if (clientNum == 1)
+                    {
+                        Vector3 spawn = new Vector3(0f, 0.5f, 0f);
+                        playerCube = Instantiate(BluePrefab, spawn, Quaternion.identity) as GameObject;
+                        playerCube.AddComponent<Cube>();
+                        otherCube = Instantiate(GreenPrefab, spawn, Quaternion.identity) as GameObject;
+                    }
+                    else if (clientNum == 2)
+                    {
+                        Vector3 spawn = new Vector3(0f, 0.5f, 0f);
+                        playerCube = Instantiate(GreenPrefab, spawn, Quaternion.identity) as GameObject;
+                        playerCube.AddComponent<Cube>();
+                        otherCube = Instantiate(BluePrefab, spawn, Quaternion.identity) as GameObject;
+                    }
+
+                    WaitingScreen.SetActive(false);
+                    IPPass = false;
+                }
+            }
+        }
+        catch (ArgumentNullException anexc)
+        {
+            Debug.Log($"ArgumentNullException: {anexc.ToString()}");
+        }
+        catch (FormatException fexc)
+        {
+            Debug.Log($"FormatException: {fexc.ToString()}");
+        }
+        catch (SocketException se)
+        {
+            Debug.Log($"SocketException: {se.ToString()}");
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"Unexpected Exception: {e.ToString()}");
+        }
+
+        SessionPass = true;
+    }
+
+    public void ClientUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UDP_outBuffer = Encoding.UTF8.GetBytes("Shutdown");
+            udpSocket.SendTo(UDP_outBuffer, remoteUDPEP);
+
+            udpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            udpSocket.Close();
+            tcpSocket.Close();
+
+            Application.Quit();
+        }
+        else
+        {
+            // Game Update //
+            // Receive Position Data
+            if (udpSocket.Available > 0)
+            {
+                try
+                {
+                    recv = udpSocket.ReceiveFrom(UDP_inBuffer, ref remoteUDPEP);
+                }
+                catch { }
+
+                if (Encoding.UTF8.GetString(UDP_inBuffer, 0, recv) == "Shutdown")
+                {
+                    udpSocket.Shutdown(SocketShutdown.Both);
+                    tcpSocket.Shutdown(SocketShutdown.Both);    
+                    udpSocket.Close();
+                    tcpSocket.Close();
+
+                    Application.Quit();
+                }
+                else
+                {
+                    Buffer.BlockCopy(UDP_inBuffer, 0, otherPosition, 0, recv);
+
+                    if (recv > 0)
+                    {
+                        otherCube.GetComponent<Transform>().position = new Vector3(otherPosition[0], otherPosition[1], otherPosition[3]);
+                    }
+                }
+            }
+
+            // Send Position Data
+            elapsedTime += Time.deltaTime;
+
+            playerPosition = new float[] {playerCube.GetComponent<Transform>().position.x, playerCube.GetComponent<Transform>().position.y, playerCube.GetComponent<Transform>().position.z};
+
+            if ((elapsedTime > 0.025f)) //Interval set at 25ms
+            {
+                Buffer.BlockCopy(playerPosition, 0, UDP_outBuffer, 0, UDP_outBuffer.Length);
+                udpSocket.SendTo(UDP_outBuffer, remoteUDPEP);
+
+                elapsedTime = 0f;
+            }
+
+            // Chat Update //
+            // Receive Chat Data
+            // Send Chat Data
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        playerCube = GameObject.Find("").transform;
+        StartScreen.SetActive(true);
+        WaitingScreen.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (serverIP != "")
+        if (IPPass && WaitingScreen.activeInHierarchy)
         {
-            Debug.Log("PASS");
+            SetupSession();
+        }
+
+        if (SessionPass)
+        {
+            ClientUpdate();
         }
     }
 }
--- DSN Midterm - Server App/Server/Program.cs
+++ DSN Midterm - Server App/Server/Program.cs
@@ -1,15 +1,150 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
-namespace Server
+public class Server
 {
-    class Program
+    private static Socket tcpSocket;
+    private static Socket udpSocket;
+    private static Socket handlerOne;
+    private static Socket handlerTwo;
+    private static EndPoint remoteUDPClient;
+    private static EndPoint clientOneUDPID;
+    private static EndPoint clientTwoUDPID;
+    private static byte[] TCP_inBuffer = new byte[32];
+    private static byte[] TCP_outBuffer = new byte[32];
+    private static byte[] UDP_inBuffer = new byte[12];
+    private static byte[] UDP_outBuffer = new byte[12];
+    private static float[] clientOnePos = new float[12];
+    private static float[] clientTwoPos = new float[12];
+    private static int recv = 0;
+
+    public static void StartServer()
     {
-        static void Main(string[] args)
+        try
         {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+            Console.WriteLine("Server name: {0} | IP: {1}", Dns.GetHostEntry(Dns.GetHostName()).HostName, ip);
+
+            IPEndPoint localTCPEP = new IPEndPoint(ip, 11111);
+            IPEndPoint localUDPEP = new IPEndPoint(ip, 11112);
+
+            tcpSocket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            udpSocket = new Socket(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint udpClient = new IPEndPoint(IPAddress.Any, 0);
+            remoteUDPClient = (EndPoint)udpClient;
+
+            tcpSocket.Bind(localTCPEP);
+            udpSocket.Bind(localUDPEP);
+
+            tcpSocket.Listen(10);
+            Console.WriteLine("Waiting for connection...");
+
+            handlerOne = tcpSocket.Accept();
+            Console.WriteLine("~ Connected! ~");
+            IPEndPoint clientOneEP = (IPEndPoint)handlerOne.RemoteEndPoint;
+            Console.WriteLine("TCP: Client {0} connected at port {1}", clientOneEP.Address, clientOneEP.Port);
+            int rec = udpSocket.ReceiveFrom(UDP_inBuffer, ref remoteUDPClient);
+            Console.WriteLine("UDP: Client {0} at {1}", Encoding.ASCII.GetString(UDP_inBuffer, 0, rec), remoteUDPClient.ToString());
+            clientOneUDPID = remoteUDPClient;
+            Console.WriteLine("--------------------------------------------");
+            handlerOne.Send(Encoding.UTF8.GetBytes("1"));
+
+            handlerTwo = tcpSocket.Accept();
+            Console.WriteLine("~ Connected! ~");
+            IPEndPoint clientTwoEP = (IPEndPoint)handlerTwo.RemoteEndPoint;
+            Console.WriteLine("TCP: Client {0} connected at port {1}", clientTwoEP.Address, clientTwoEP.Port);
+            rec = udpSocket.ReceiveFrom(UDP_inBuffer, ref remoteUDPClient);
+            Console.WriteLine("UDP: Client {0} at {1}", Encoding.ASCII.GetString(UDP_inBuffer, 0, rec), remoteUDPClient.ToString());
+            clientTwoUDPID = remoteUDPClient;
+            Console.WriteLine("--------------------------------------------");
+            handlerTwo.Send(Encoding.UTF8.GetBytes("2"));
+
+            bool cos = false, cts = false;
+            int recv = handlerOne.Receive(TCP_inBuffer);
+            cos = Encoding.UTF8.GetString(TCP_inBuffer, 0, recv) == "Ready";
+            recv = handlerTwo.Receive(TCP_inBuffer);
+            cts = Encoding.UTF8.GetString(TCP_inBuffer, 0, recv) == "Ready";
+
+            if (cos && cts)
+            {
+                handlerOne.Send(Encoding.UTF8.GetBytes("StartSession"));
+                handlerTwo.Send(Encoding.UTF8.GetBytes("StartSession"));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Unexpected Exception: {0}", e.ToString());
+        }
+    }
+
+    public static void ServerUpdate()
+    {
+        // Game Updates //
+        if (udpSocket.Available > 0)
+        {
+            try
+            {
+                recv = udpSocket.ReceiveFrom(UDP_inBuffer, ref remoteUDPClient);
+            }
+            catch { }
+
+            if (remoteUDPClient.ToString() == clientOneUDPID.ToString()) //Receive Client 1 Position > Send to Client 2
+            {
+                Buffer.BlockCopy(UDP_inBuffer, 0, clientOnePos, 0, recv);
+
+                Console.WriteLine("Received {0},{1},{2} from {3} ... Sent {0},{1},{2} to {4} ...", 
+                    clientOnePos[0], clientOnePos[1], clientOnePos[2], clientOneUDPID, clientTwoUDPID);
+
+                Buffer.BlockCopy(clientOnePos, 0, UDP_outBuffer, 0, UDP_outBuffer.Length);
+                udpSocket.SendTo(UDP_outBuffer, clientTwoUDPID);
+            }
+            else if (remoteUDPClient.ToString() == clientTwoUDPID.ToString()) //Receive Client 2 Position > Send to Client 1
+            {
+                Buffer.BlockCopy(UDP_inBuffer, 0, clientTwoPos, 0, recv);
+
+                Console.WriteLine("Received {0},{1},{2} from {3} ... Sent {0},{1},{2} to {4} ...",
+                    clientTwoPos[0], clientTwoPos[1], clientTwoPos[2], clientTwoUDPID, clientOneUDPID);
+
+                Buffer.BlockCopy(clientTwoPos, 0, UDP_outBuffer, 0, UDP_outBuffer.Length);
+                udpSocket.SendTo(UDP_outBuffer, clientOneUDPID);
+            }
+        }
+
+        // Chat Updates //
+        if (handlerOne.Available > 0)
+        {
+            try
+            {
+                recv = handlerOne.Receive(TCP_inBuffer);
+            }
+            catch { }
+
+            if (recv > 0)
+            {
+                IPEndPoint clientOneEP = (IPEndPoint)handlerOne.RemoteEndPoint;
+                Console.WriteLine("Received message \"{0}\" from {1} ...", Encoding.UTF8.GetString(TCP_inBuffer, 0, recv), clientOneEP.Address);
+
+
+            }
+        }
+    }
+
+    public static void Main(String[] args)
+    {
+        StartServer();
+
+        tcpSocket.Blocking = false;
+        handlerOne.Blocking = false;
+        handlerTwo.Blocking = false;
+        udpSocket.Blocking = false;
+
+        while (true)
+        {
+            ServerUpdate();
         }
     }
 }
